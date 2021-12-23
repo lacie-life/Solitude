5373db58ea0b931b8bb682d32bff81bfce7ac207
lacie <00sao00ios00@gmail.com>
2021-12-23 23:55:00 +0700

    [Add] KMS_IMUData and KMS_IMUPreintegrator class

--- Code/SLAM/src/common/IMUdata.cpp
+++ Code/SLAM/src/common/IMUdata.cpp
@@ -2,24 +2,24 @@
 
 
 namespace kms_slam {
-//imu.gyrVar: 1.2e-3 #4e-3    # 1.2e-3
-//imu.accVar: 8e-3   #2e-2    # 8.0e-3
-//imu.gyrBiasVar: 4.0e-6    # 1e-9
-//imu.accBiasVar: 4.0e-5    # 1e-8
+    //imu.gyrVar: 1.2e-3 #4e-3    # 1.2e-3
+    //imu.accVar: 8e-3   #2e-2    # 8.0e-3
+    //imu.gyrBiasVar: 4.0e-6    # 1e-9
+    //imu.accBiasVar: 4.0e-5    # 1e-8
 
-// covariance of measurement
-// From continuous noise_density of dataset sigma_g/sigma_a   rad/s/sqrt(Hz) -- m/s^2/sqrt(Hz)
+    // covariance of measurement
+    // From continuous noise_density of dataset sigma_g/sigma_a   rad/s/sqrt(Hz) -- m/s^2/sqrt(Hz)
 
-/**
- * For EuRoc dataset, according to V1_01_easy/imu0/sensor.yaml
- * The params:
- * sigma_g: 1.6968e-4       rad / s / sqrt(Hz)
- * sigma_gw: 1.9393e-5      rad / s^2 / sqrt(Hz)
- * sigma_a: 2.0e-3          m / s^2 / sqrt(Hz)
- * sigma_aw: 3.0e-3         m / s^3 / sqrt(Hz)
- */
+    /**
+     * For EuRoc dataset, according to V1_01_easy/imu0/sensor.yaml
+     * The params:
+     * sigma_g: 1.6968e-4       rad / s / sqrt(Hz)
+     * sigma_gw: 1.9393e-5      rad / s^2 / sqrt(Hz)
+     * sigma_a: 2.0e-3          m / s^2 / sqrt(Hz)
+     * sigma_aw: 3.0e-3         m / s^3 / sqrt(Hz)
+     */
 
-// *1e3/*1e2 chosen by experiments
+    // *1e3/*1e2 chosen by experiments
     double IMUData::_gyrBiasRw2 = 2.0e-5 * 2.0e-5/**10*/;  //2e-12*1e3
     double IMUData::_accBiasRw2 = 5.0e-3 * 5.0e-3/**10*/;  //4.5e-8*1e2
 
@@ -39,9 +39,38 @@ namespace kms_slam {
     }
 
 
-//IMUData::IMUData(const IMUData& imu) :
-//    _g(imu._g), _a(imu._a), _t(imu._t)
-//{
-//}
+    //IMUData::IMUData(const IMUData& imu) :
+    //    _g(imu._g), _a(imu._a), _t(imu._t)
+    //{
+    //}
+
+//-------------------------------KMS_IMUData Class---------------------------------------/
+
+    //Initialize those static member variables
+    //Set the IMU parameters, cotinious parameters(defaul set for Euroc dataset)
+    double KMS_IMUData::sigma_gyroscope_ = 1.6968e-4;
+    double KMS_IMUData::sigma_accelerometer_ = 2.0e-3;
+
+    double KMS_IMUData::sigma_gyroscope_walk_ = 1.9393E-5;
+    double KMS_IMUData::sigma_accelerometer_walk_ = 3.0e-3;
+
+    double KMS_IMUData::delta_timestamp_ = 0.005;
+
+    //Calculate discrete IMU sensor parameters
+    double KMS_IMUData::gyroscope_noise_rw2_ = KMS_IMUData::sigma_gyroscope_ * KMS_IMUData::sigma_gyroscope_/KMS_IMUData::delta_timestamp_;
+    double KMS_IMUData::accelerometer_noise_rw2_ = KMS_IMUData::sigma_accelerometer_ *  KMS_IMUData::sigma_accelerometer_/KMS_IMUData::delta_timestamp_;
+
+    Eigen::Matrix3d KMS_IMUData::gyroscope_meas_covariance_ = Eigen::Matrix3d::Identity() *  KMS_IMUData::gyroscope_noise_rw2_;
+    Eigen::Matrix3d KMS_IMUData::accelerometer_meas_covariance_ =  Eigen::Matrix3d::Identity() * KMS_IMUData::accelerometer_noise_rw2_;
+
+    double KMS_IMUData::gyroscope_bias_rw2_ = KMS_IMUData::sigma_gyroscope_walk_ * KMS_IMUData::sigma_gyroscope_walk_ * KMS_IMUData::delta_timestamp_;
+    double KMS_IMUData::accelerometer_bias_rw2_ = KMS_IMUData::sigma_accelerometer_walk_ * KMS_IMUData::sigma_accelerometer_walk_ * KMS_IMUData::delta_timestamp_;
+
+    Eigen::Matrix3d KMS_IMUData::gyroscope_bias_rw_covariance_ = Eigen::Matrix3d::Identity() * KMS_IMUData::gyroscope_bias_rw2_;
+    Eigen::Matrix3d KMS_IMUData::accelerometer_bias_rw_covariance_ = Eigen::Matrix3d::Identity() * KMS_IMUData::accelerometer_bias_rw2_;
+
+    KMS_IMUData::KMS_IMUData(const double &gx, const double &gy, const double &gz,
+                     const double &ax, const double &ay, const double &az,
+                     const double &t) : gyroscope_(gx,gy,gz), accelerometer_(ax,ay,az), timestamp_(t) {}
 
 } // namespace kms_slam
--- Code/SLAM/src/optimizer/IMUPreintegrator.cpp
+++ Code/SLAM/src/optimizer/IMUPreintegrator.cpp
@@ -71,9 +71,9 @@ namespace kms_slam {
 
     }
 
-// incrementally update 1)delta measurements, 2)jacobians, 3)covariance matrix
-// acc: acc_measurement - bias_a, last measurement!! not current measurement
-// omega: gyro_measurement - bias_g, last measurement!! not current measurement
+    // incrementally update 1)delta measurements, 2)jacobians, 3)covariance matrix
+    // acc: acc_measurement - bias_a, last measurement!! not current measurement
+    // omega: gyro_measurement - bias_g, last measurement!! not current measurement
     void IMUPreintegrator::update(const Vector3d &omega, const Vector3d &acc, const double &dt) {
         double dt2 = dt * dt;
 
@@ -133,6 +133,103 @@ namespace kms_slam {
         // delta time
         _delta_time += dt;
 
+    }
+
+    // ---------------------------KMS_IMUPreintegrator class----------------------//
+    KMS_IMUPreintegrator::KMS_IMUPreintegrator(){
+        delta_p_.setZero();
+        delta_v_.setZero();
+        delta_rot_.setIdentity();
+
+        jacobian_v_bias_gyroscope_.setZero();
+        jacobian_v_bias_accelerometer_.setZero();
+
+        jacobian_p_bias_gyroscope_.setZero();
+        jacobian_p_bias_accelerometer_.setZero();
+
+        jacobian_rot_bias_gyroscope_.setZero();
+
+        cov_p_v_rot_.setZero();
+
+        delta_time_ = 0.0;
+    }
+
+    KMS_IMUPreintegrator::KMS_IMUPreintegrator(const KMS_IMUPreintegrator &preintegrator) :
+            delta_p_(preintegrator.delta_p_), delta_v_(preintegrator.delta_v_), delta_rot_(preintegrator.delta_rot_),
+            jacobian_p_bias_gyroscope_(),
+            jacobian_p_bias_accelerometer_(),
+            jacobian_v_bias_gyroscope_(),
+            jacobian_v_bias_accelerometer_(),
+            jacobian_rot_bias_gyroscope_(),
+            cov_p_v_rot_(), delta_time_(preintegrator.delta_time_) {}
+
+    void KMS_IMUPreintegrator::Update(const Eigen::Vector3d &omega, const Eigen::Vector3d &acc,const double &delta_timestamp_){
+        double dt2 = delta_timestamp_*delta_timestamp_;
+
+        //Step-1: Calculate IMU pre-integral rotation increment and Jacobian
+        Eigen::Matrix3d dR = Sophus::SO3::exp(omega*delta_timestamp_).matrix();
+        Eigen::Matrix3d Jr_R = Sophus::SO3::JacobianR(omega*delta_timestamp_);
+
+        //Step 2: Calculate the uncertainity variance of the noise error of the IMU pre-integrator observation equation
+        Eigen::Matrix3d I3x3;
+        I3x3.setIdentity();
+
+        Matrix9d A;
+        A.setIdentity();
+        //  The block size is 3x3, the starting position (6,6)
+        A.block<3,3>(6,6) = dR.transpose();
+        A.block<3,3>(3,6) = -delta_rot_ * Sophus::SO3::hat(acc) * delta_timestamp_;
+        A.block<3,3>(0,6) = -0.5* delta_rot_ *Sophus::SO3::hat(acc) * dt2;
+        A.block<3,3>(0,3) = I3x3 *delta_timestamp_;
+
+        //Split the B in the paper into two parts
+        Eigen::Matrix<double,9,3> Bg;
+        Bg.setZero();
+        Bg.block<3,3>(6,0) = Jr_R * delta_timestamp_;
+
+        Eigen::Matrix<double,9,3> Ca;
+        Ca.setZero();
+        Ca.block<3,3>(3,0) = delta_rot_ * delta_timestamp_;
+        Ca.block<3,3>(0,0) = 0.5 * delta_rot_ * dt2;
+
+        cov_p_v_rot_ = A * cov_p_v_rot_ * A.transpose() +
+                       Bg * KMS_IMUData::gyroscope_meas_covariance_ * Bg.transpose() +
+                       Ca * KMS_IMUData::accelerometer_meas_covariance_ * Ca.transpose();
+
+        //Step-3: Calculate Jacobian, used to correct the biased IMU pre-integrated measurement value
+
+        //Recursion based on supplementary material formula
+        jacobian_p_bias_accelerometer_ += jacobian_v_bias_accelerometer_ * delta_timestamp_ -0.5 * delta_rot_ * dt2;
+        jacobian_p_bias_gyroscope_ += jacobian_v_bias_gyroscope_ * delta_timestamp_ - 0.5 * delta_rot_ * Sophus::SO3::hat(acc) * jacobian_rot_bias_gyroscope_ * dt2;
+
+        jacobian_v_bias_accelerometer_ += -delta_rot_ * delta_timestamp_;
+        jacobian_v_bias_gyroscope_ += -delta_rot_ * Sophus::SO3::hat(acc) * jacobian_rot_bias_gyroscope_ * delta_timestamp_;
+
+        jacobian_rot_bias_gyroscope_ = dR.transpose() * jacobian_rot_bias_gyroscope_ - Jr_R * delta_timestamp_;
+
+        //Step-4: Calculate IMU pre-integrated measurement value
+        delta_p_ += delta_v_*delta_timestamp_ + 0.5 * delta_rot_ * acc * dt2;
+        delta_v_ += delta_rot_ * acc * delta_timestamp_;
+        delta_rot_ += NormalizedRotationM(delta_rot_*dR);
+        delta_time_ += delta_timestamp_;
+    }
+
+    void KMS_IMUPreintegrator::Reset(){
+        delta_p_.setZero();
+        delta_v_.setZero();
+        delta_rot_.setIdentity();
+
+        jacobian_v_bias_gyroscope_.setZero();
+        jacobian_v_bias_accelerometer_.setZero();
+
+        jacobian_p_bias_gyroscope_.setZero();
+        jacobian_p_bias_accelerometer_.setZero();
+
+        jacobian_rot_bias_gyroscope_.setZero();
+
+        cov_p_v_rot_.setZero();
+
+        delta_time_ = 0.0;
     }
 
 } // namespace kms_slam
