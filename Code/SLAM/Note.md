e278ffdbe427658fa34af38fb19be92ffa96b998
lacie <00sao00ios00@gmail.com>
2021-12-25 21:59:57 +0700

    [Update] build success in Ubuntu 18.04

---
+++ Code/SLAM/Note.md
@@ -0,0 +1,4 @@
+# Note about errors
+
+## Glew
+https://github.com/raulmur/ORB_SLAM2/issues/210
--- Code/SLAM/build.sh
+++ Code/SLAM/build.sh
@@ -6,14 +6,14 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../g2o
-
-echo "Configuring and building Thirdparty/g2o ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+#cd ../../g2o
+#
+#echo "Configuring and building Thirdparty/g2o ..."
+#
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j
 
 #cd ../../../
 #
