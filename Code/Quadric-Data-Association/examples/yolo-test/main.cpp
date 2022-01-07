6ba9db9defb3333d939c9242bd47349f0470ac15
lacie-life <00sao00ios00@gmail.com>
2022-01-07 21:52:38 +0700

    [Fixed] Yolo model

--- Code/Quadric-Data-Association/examples/yolo-test/main.cpp
+++ Code/Quadric-Data-Association/examples/yolo-test/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #include "gtsam_quadrics/models/yolo.h"
 
@@ -12,11 +15,11 @@ int main(void) {
     std::cout << "Hello world !!!" << std::endl;
     // Read the image file as
     // imread("default.jpg");
-    cv::Mat image = cv::imread("/home/lacie/Github/My-Researches/Code/Quadrics-Data-Association/data/test.jpg",
+    cv::Mat image = cv::imread("/home/lacie/Github/My-Researches/Code/Quadric-Data-Association/data/test.jpg",
                            cv::IMREAD_ANYCOLOR);
 
-    Yolo model = Yolo("/home/lacie/Github/My-Researches/Code/Quadrics-Data-Association/models/yolov5s.torchscript.pt",
-                      "/home/lacie/Github/My-Researches/Code/Quadrics-Data-Association/models/coco.names");
+    Yolo model = Yolo("/home/lacie/Github/My-Researches/Code/Quadric-Data-Association/models/yolov5s.torchscript.pt",
+                      "/home/lacie/Github/My-Researches/Code/Quadric-Data-Association/models/coco.names");
 
     // Error Handling
     if (image.empty()) {
