4d6c8f804ac8ac5e68e3b312dc3f3b955361f4ba
lacie-life <00sao00ios00@gmail.com>
2022-01-16 19:35:41 +0700

    [Update] update place recognition test

--- Code/Quadric-Data-Association/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lacie/Github/My-Researches/Code/Quadric-Data-Association
-# Build directory: /home/lacie/Github/My-Researches/Code/Quadric-Data-Association/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples/yolo-test")
--- Code/Quadric-Data-Association/cmake-build-debug/examples/yolo-test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lacie/Github/My-Researches/Code/Quadric-Data-Association/examples/yolo-test
-# Build directory: /home/lacie/Github/My-Researches/Code/Quadric-Data-Association/cmake-build-debug/examples/yolo-test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
